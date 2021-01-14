5c401908d7f471dd883a141e152f851c1300bb2c
Matt <mattromo27@gmail.com>
2021-01-14 18:37:32 -0500

    First commit

---
+++ WebSocket_ChatClient/WebSocket_ChatClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocket_ChatClientApp.swift
+//  WebSocket_ChatClient
+//
+//  Created by Matt Roumeliotis on 1/14/21.
+//
+
+import SwiftUI
+
+@main
+struct WebSocket_ChatClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
